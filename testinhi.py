3cf7e33e730681e8bc2fba3028c7cee04661d2f8
joaorobsonR <jrobsonramalho@gmail.com>
2023-06-20 21:35:09 -0300

    segundo testinho

--- testinhi.py
+++ testinhi.py
@@ -3,3 +3,14 @@ while cont < 10:
     cont += 1
     print(cont)
 
+def testinhoFor(x, y):
+    cont = 0
+    for i in range(x, y):
+        i += 1
+        print(i)
+        cont = (x + y) * y
+    
+    return cont
+
+resultado = testinhoFor(0, 10)
+print(resultado)
