a14367598826f05560f259763b3b5ae9c58ab4d1
joaorobsonR <jrobsonramalho@gmail.com>
2023-06-22 20:42:47 -0300

    urls e models

--- django3/crud/models.py
+++ django3/crud/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    servico = models.CharField(max_length=20)
+    telefone = models.CharField(max_length=30)
+    salario = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
